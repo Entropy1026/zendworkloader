8e81ee9a97d0285a42df22e0f7048573ea14244e
marjohnleem <marjohnleedelavictoria@gmail.com>
2019-04-24 16:25:55 -0700

    FIRST COMMIT

---
+++ module/Workload/config/module.config.php
@@ -0,0 +1,41 @@
+<?php 
+namespace Workload;
+
+use Zend\Router\Http\Literal;
+use Zend\ServiceManager\Factory\InvokableFactory;
+
+return [
+    
+    'controllers' => [
+        'factories' => [
+            Controller\WorklistController::class => InvokableFactory::class,
+        ],
+    ],
+    // This lines opens the configuration for the RouteManager
+    'router' => [
+        // Open configuration for all possible routes
+        'routes' => [
+            // Define a new route called "blog"
+            'workload' => [
+                // Define a "literal" route type:
+                'type' => Literal::class,
+                // Configure the route itself
+                'options' => [
+                    // Listen to "/blog" as uri:
+                    'route' => '/workload',
+                    // Define default controller and action to be called when
+                    // this route is matched
+                    'defaults' => [
+                        'controller' => Controller\WorklistController::class,
+                        'action'     => 'index',
+                    ],
+                ],
+            ],
+        ],
+    ],
+    'view_manager' => [
+        'template_path_stack' => [
+            __DIR__ . '/../view',
+        ],
+    ],
+];
---
+++ module/Workload/src/Controller/WorklistController.php
@@ -0,0 +1,9 @@
+<?php
+// In module/Blog/src/Controller/ListController.php:
+namespace Workload\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+class WorklistController extends AbstractActionController
+{
+   
+}
